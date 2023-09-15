f5b36644dcc4c1805906ad808e3f2874ca2bb1da
coolfoal <coolfoal@outlook.com>
2023-09-15 23:45:31 +0800

    fix

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -10,12 +10,15 @@ from time import ctime
 
 
 @route('/')
-def index(name):
+def index():
     return template('<b>Time: {{thetime}}</b>!', thetime=ctime())
 
 @route('/hello/<name>')
 def hello(name):
     return template('<b>Hello {{name}}</b>!', name=name)
 
-#run(host='localhost', port=8080)
-application = default_app()
+
+if __name__ == '__main__':
+    run(host='localhost', port=8080)
+else:
+    application = default_app()
