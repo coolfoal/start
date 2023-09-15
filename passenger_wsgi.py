f3f333a76978562d3892994a4e7e8c1b7ece644f
coolfoal <coolfoal@outlook.com>
2023-09-15 18:10:29 +0800

    fix path

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -5,11 +5,17 @@ Created on Fri Sep 15 10:24:36 2023
 @author: coolfoal
 """
 
-from bottle import route, run, template
+from bottle import route, run, template, default_app
+from time import ctime
+
+
+@route('/')
+def index(name):
+    return template('<b>Time: {{thetime}}</b>!', thetime=ctime()))
 
 @route('/hello/<name>')
-def index(name):
+def hello(name):
     return template('<b>Hello {{name}}</b>!', name=name)
 
 #run(host='localhost', port=8080)
-application = bottle.default_app()
+application = default_app()
